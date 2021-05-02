b2b1ff5d7edb3d1027d3c7c26635c0f62492df4b
shir <bhshir@gmail.com>
2021-05-02 17:47:30 +0300

    fix validation

--- server/modules/user/user.model.mjs
+++ server/modules/user/user.model.mjs
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
-
+const validateEmail = (email) => {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+  };
 const UserSchema = new Schema({
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
