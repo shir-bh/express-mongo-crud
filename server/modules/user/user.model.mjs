1b9f95f2ef52b7e8412a4003a4476e7cc02f8a5d
shir <bhshir@gmail.com>
2021-05-02 15:14:36 +0300

    db validation

--- server/modules/user/user.model.mjs
+++ server/modules/user/user.model.mjs
@@ -2,10 +2,20 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const UserSchema = new Schema({
-    first_name  : String,
-    last_name   : String,
-    email       : String,
-    phone       : String
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: "Email is required",
+        validate: [validateEmail, "enter a valid email"],
+        match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "enter a valid email",
+        ],
+    },
 });
   
 export default model('user',UserSchema);
